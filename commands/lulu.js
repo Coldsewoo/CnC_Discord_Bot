9e557b6d94128a7eb0f67b05ff5086302d3ba097
coldsewoo <coldsewoo@gmail.com>
2018-11-08 18:43:01 +0900

    lulu.js

--- commands/lulu.js
+++ commands/lulu.js
@@ -1,4 +1,29 @@
 
 exports.run = (client, message, args) => {
-  message.channel.send("기여어!");
+  var clientDate = new Date();
+  var timezone = new Date(clientDate.getTime() + (clientDate.getTimezoneOffset() * 60000));
+  var timezone2 = new Date(clientDate.getTime() + (clientDate.getTimezoneOffset() * 60000) + 32400000);
+  var timezoneoffset = new Date(clientDate.getTimezoneOffset());
+  message.channel.send(`${clientDate} clientDate`);
+  message.channel.send(`${timezoneoffset*60000} timezoneoffset*60000`);
+  message.channel.send(`${timezone} timezone`);
+  message.channel.send(`${timezone2} timezone2`);
+
+  var dates = [
+    timezone.getFullYear(),
+    parseInt(timezone.getMonth())+1,
+    timezone.getDate(),
+    timezone.getHours(),
+    timezone.getMinutes()
+  ];
+  var dates2 = [
+    timezone2.getFullYear(),
+    parseInt(timezone2.getMonth())+1,
+    timezone2.getDate(),
+    timezone2.getHours(),
+    timezone2.getMinutes()
+  ]
+
+  message.channel.send(`${dates[0]}/${dates[1]}/${dates[2]}/${dates[3]}/${dates[4]} timezone`);
+  message.channel.send(`${dates2[0]}/${dates2[1]}/${dates2[2]}/${dates2[3]}/${dates2[4]} timezone2`);
 }
