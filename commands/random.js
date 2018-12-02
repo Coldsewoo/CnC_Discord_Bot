457bab7215e286110c87c38177bd9a2d68c7463a
coldsewoo <coldsewoo@gmail.com>
2018-12-02 22:35:50 +0900

    bigger picture on random

--- commands/random.js
+++ commands/random.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 exports.run = (client, message, args) => {
 	try {
 		const requestItem = args;
-		get('https://loremflickr.com/320/240/'+requestItem).then(res => {
+		get('https://loremflickr.com/448/336/'+requestItem).then(res => {
 		const embed = new Discord.RichEmbed()
 				.setImage('https://loremflickr.com'+res.request.path);
 			return message.channel.send({ embed });
