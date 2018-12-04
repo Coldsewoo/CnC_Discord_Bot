ed65b411af2e931c101ff08849e204335495919a
coldsewoo <coldsewoo@gmail.com>
2018-12-04 16:49:22 +0900

    random chromie XD

--- commands/random.js
+++ commands/random.js
@@ -4,13 +4,19 @@ const Discord = require('discord.js');
 exports.run = (client, message, args) => {
 	try {
 		const requestItem = args;
+		var path;
 		get('https://loremflickr.com/448/336/'+requestItem).then(res => {
+			if(res.request.path == "/cache/resized/defaultImage.small_448_336_nofilter.jpg"){
+				path = 'https://i.postimg.cc/RZN8F0VP/chromie2.jpg';
+			} else {
+				path = 'https://loremflickr.com'+res.request.path;
+			}
 		const embed = new Discord.RichEmbed()
-				.setImage('https://loremflickr.com'+res.request.path);
+				.setImage(path);
 			return message.channel.send({ embed });
 		});
 	}
 	catch(err) {
-		return message.channel.send(err.stack);
+		message.reply("Error! pls use only English letters");
 	}
 };
