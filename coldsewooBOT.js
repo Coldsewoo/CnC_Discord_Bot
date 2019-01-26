954f4bcf5f7e1e0f0c3fc9cd13464ced8602aa23
coldsewoo <coldsewoo@gmail.com>
2019-01-26 20:46:54 +0900

    automatically fetches code update

--- coldsewooBOT.js
+++ coldsewooBOT.js
@@ -1,6 +1,9 @@
 const Discord = require("discord.js");
 const Enmap = require("enmap");
 const fs = require("fs");
+const {
+  get
+} = require('snekfetch');
 
 const client = new Discord.Client();
 const config = require("./config.json");
@@ -10,7 +13,7 @@ client.on('ready', () => {
   client.user.setActivity('Use ~help for more info XD', { type: 'PLAYING' });
 });
 
-fs.readdir("./events/", (err,files) => {
+fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
   files.forEach(file => {
     const event = require(`./events/${file}`);
@@ -21,15 +24,55 @@ fs.readdir("./events/", (err,files) => {
 
 client.commands = new Enmap();
 
-fs.readdir("./commands/", (err,files) => {
-if (err) return console.error(err);
-files.forEach(file => {
-  if (!file.endsWith(".js")) return;
-  let props = require(`./commands/${file}`);
-  let commandName = file.split(".")[0];
-  console.log(`Attempting to load command ${commandName}`);
-  client.commands.set(commandName, props);
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    console.log(`Attempting to load command ${commandName}`);
+    client.commands.set(commandName, props);
+  });
 });
-});
+
+setInterval(function () {
+  var facebookArray = new Array();
+  try {
+    get('https://www.facebook.com/pg/Idle-Online-Universe-IOU-RPG-320106674851481/posts/?ref=page_internal').then(res => {
+      var content = res.body.toString();
+      var codeFinder = "Here are your codes"
+      var codeEndFinder = "Cheers, Lynn"
+      var codeIndex = content.indexOf(codeFinder);
+      var codeEndIndex = content.indexOf(codeEndFinder);
+      var codes = content.substring(codeIndex, codeEndIndex)
+      var br = "<br />"
+      var codesArray = codes.split(br);
+      for (let i = 0; i < 3; i++) {
+        codesArray[i] = codesArray[i].trim();
+        facebookArray.push(codesArray[i]);
+      }
+      return facebookArray;
+    }).then(facebookArray => {
+      var msgArray = new Array();
+      const channelId = "453517489561665536";
+      const codeChannel = client.channels.get(channelId)
+
+      codeChannel.fetchMessages({
+        limit: 1
+      }).then(collected => {
+        collected.forEach(msg => {
+          msgArray = msg.content.split(/\n/);
+        })
+        return Promise.resolve([facebookArray, msgArray])
+      }).then(([facebookArray, msgArray]) => {
+        if (!facebookArray.includes(msgArray[1])) {
+          codeChannel.send(facebookArray);
+        }
+      })
+    })
+  } catch (err) {
+    console.log(err);
+  }
+}, 30 * 60 * 1000);
 
 client.login(config.token);
