c4dee5d68389a1391cd0c2094fa2138b3afd3d4c
coldsewoo <coldsewoo@gmail.com>
2019-03-18 04:56:15 +0900

    help page text modified

--- commands/Util/help.js
+++ commands/Util/help.js
@@ -1,9 +1,10 @@
-const globalVar = require(__basedir + '/globalVar.js')
+// eslint-disable-next-line no-undef
+const globalVar = require(__basedir + '/globalVar.js');
 const Global = globalVar.Global;
 
 exports.run = (client, message, args) => {
-	//const guildcolor = ['14713377', '7382744', '951659', '9984690', '3407751', '16398164', '16312092'];
-	//const guildname = ['BR', 'CS', 'The Collectives', 'Imaginarium', 'Fresh Air', 'Always Online'];
+	// const guildcolor = ['14713377', '7382744', '951659', '9984690', '3407751', '16398164', '16312092'];
+	// const guildname = ['BR', 'CS', 'The Collectives', 'Imaginarium', 'Fresh Air', 'Always Online'];
 	let color;
 	for (let i = 0; i < 6; i++) {
 		if (message.member.roles.find(role => role.name === Global.guildRole[i])) {
@@ -24,35 +25,30 @@ exports.run = (client, message, args) => {
 				name: 'Available Commands (use ~ before the desired command)',
 				value: `
 __*For Everyone*__
-***p*** : **Calculate stone required for guild personal** **from/to**
- *See ~p help*
-***g*** : **Calculate stone required for guild level** **from/to**
- *See ~g help*
+***p*** : **Calculates exp required for personal lv**
+***pl*** : **Calculates the personal level you can reach with given personal EXP**
+***g*** : **Calculates stone required for guild lv**
+***gl*** : **Calculates the guild building level you can reach with given stone amount**
 ***Guides*** : **Useful Sheets & Guides on IOU**
-***Cat*** : **Random cat XD**
-***Puppy*** : **Random puppy XDXD**
-***Random*** : **Random everything XDXDXD**
+***Random*** : **Random pictures!**
 
 __*For CnC members*__
 ***Update*** : **Update guild information**
-*Updated Information is from CnC Guild's Utility Sheet*
+*the Information is from CnC Guild's Utility Sheet*
 ***Info*** : **Guild Level information**
- *See ~info help*
 ***Bonus*** : **Guild Bonus information**
- *See ~Bonus help*
 ***Compare*** : **Compare level & bonus information each guild**
- *See ~Compare help*
 ***Queue*** : **Shows queue list, In and Out**
 *(__Only for INNER-GUILD PUSHES channels!__)*
- *See ~Queue help*
 
 __*For ADMINS*__
-***ClearCommands [Number]*** - **Delete bot related request in this channel**
+***clear [Number]*** - **Delete messages in this channel**
+***clearcmds [Number]*** - **Delete bot related request in this channel**
 
 **Contact me if you have any question or suggestion!**
 **https://github.com/Coldsewoo/CnC_Discord_Bot**\n\n\n
           `,
-			},],
+			}],
 
 			footer: {
 				icon_url: 'https://i.postimg.cc/rmxgPCzB/2018-11-07-2-54-39.png',
@@ -60,7 +56,7 @@ __*For ADMINS*__
 
 			},
 		},
-	}).catch(function (err) {
+	}).catch(function(err) {
 		console.error(err);
 	});
 };
